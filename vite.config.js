5c1f344c68e533eb62b77e3a82c0354336ef349b
Juanalfaros <juan.alfaro.s@live.cl>
2025-07-22 12:08:32 -0400

    update

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'node:path';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../public',
+    emptyOutDir: true
+  },
+  plugins: [
+    createHtmlPlugin({ minify: true }),
+    viteStaticCopy({
+      targets: [{ src: 'assets/**/*', dest: 'assets' }]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets')
+    }
+  }
+});
